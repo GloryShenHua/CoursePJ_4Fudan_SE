b2adcbdb3b3447ab900792c2a0d8a26e7f57dcee
Xu Runjie <23302010022@m.fudan.edu.cn>
2025-03-14 22:13:10 +0800

    hotfix: use this file to avoid interception from Spring Security

--- src/main/java/com/example/springboot101/config/WebSecurityConfig.java
+++ src/main/java/com/example/springboot101/config/WebSecurityConfig.java
@@ -1,4 +1,25 @@
 package com.example.springboot101.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 配置 CSRF
+                .csrf(csrf -> csrf.disable())
+                // 配置 URL 访问权限
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
 }
+
